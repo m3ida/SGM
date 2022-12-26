3bc826c8595c1410205d8b5a71db573c2a89ec38
Eri <ericarodrigues884@gmail.com>
2022-12-26 15:05:50 +0000

    a bunch of images

--- src/coords/cities.js
+++ src/coords/cities.js
@@ -2,7 +2,96 @@ import ff1 from '../img/1/1.png'
 import ff2 from '../img/1/2.png'
 import ff3 from '../img/1/3.png'
 import ff4 from '../img/1/4.png'
-
+import macucar1 from '../img/2/1.png'
+import macucar2 from '../img/2/2.png'
+import macucar3 from '../img/2/3.png'
+import macucar4 from '../img/2/4.png'
+import macucar5 from '../img/2/5.png'
+import jbotan1 from '../img/3/1.png'
+import jbotan2 from '../img/3/2.png'
+import jbotan3 from '../img/3/3.png'
+import jbotan4 from '../img/3/4.png'
+import jbotan5 from '../img/3/5.png'
+import jbotan6 from '../img/3/6.png'
+import wine1 from '../img/4/1.jpg'
+import wine2 from '../img/4/2.jpg'
+import wine3 from '../img/4/3.jpg'
+import wine4 from '../img/4/4.jpg'
+import wine5 from '../img/4/5.jpg'
+import sjorge1 from '../img/5/1.jpg'
+import sjorge2 from '../img/5/2.png'
+import sjorge3 from '../img/5/3.jpg'
+import sjorge4 from '../img/5/4.png'
+import sjorge5 from '../img/5/5.png'
+import ptema1 from '../img/6/1.jpg'
+import ptema2 from '../img/6/2.png'
+import ptema3 from '../img/6/3.jpg'
+import ptema4 from '../img/6/4.jpg'
+import ptema5 from '../img/6/5.png'
+import ptema6 from '../img/6/6.png'
+import ptema7 from '../img/6/7.png'
+import ptema8 from '../img/6/8.png'
+import cristo1 from '../img/7/1.jpg'
+import cristo2 from '../img/7/2.jpg'
+import cristo3 from '../img/7/3.jpg'
+import cristo4 from '../img/7/4.jpg'
+import cristo5 from '../img/7/5.JPG'
+import cristo6 from '../img/7/6.png'
+import cristo7 from '../img/7/7.png'
+import cjesuit1 from '../img/8/1.png'
+import cjesuit2 from '../img/8/2.png'
+import cjesuit3 from '../img/8/3.png'
+import cabogi1 from '../img/9/1.png'
+import cabogi2 from '../img/9/2.png'
+import cabogi3 from '../img/9/3.png'
+import cabogi4 from '../img/9/4.png'
+import eiraserrado1 from '../img/10/1.png'
+import eiraserrado2 from '../img/10/2.png'
+import eiraserrado3 from '../img/10/3.png'
+import eiraserrado4 from '../img/10/4.png'
+import picoar1 from '../img/11/1.png'
+import picoar2 from '../img/11/2.png'
+import picoar3 from '../img/11/3.png'
+import corposanto1 from '../img/12/1.png'
+import corposanto2 from '../img/12/2.png'
+import santanacasa1 from '../img/13/1.png'
+import santanacasa2 from '../img/13/2.png'
+import santanacasa3 from '../img/13/3.png'
+import potascidade1 from '../img/14/1.png'
+import potascidade2 from '../img/14/2.png'
+import saolourenco1 from '../img/15/1.png'
+import saolourenco2 from '../img/15/2.png'
+import saolourenco3 from '../img/15/3.png'
+import saolourenco4 from '../img/15/4.png'
+import banger1 from '../img/16/1.png'
+import banger2 from '../img/16/2.png'
+import zarco1 from '../img/17/1.png'
+import zarco2 from '../img/17/2.png'
+import zarco3 from '../img/17/3.png'
+import camarasantana1 from '../img/18/1.png'
+import camarasantana2 from '../img/18/2.png'
+/* import ff4 from '../img/1/4.png'
+import ff1 from '../img/1/1.png'
+import ff2 from '../img/1/2.png'
+import ff3 from '../img/1/3.png'
+import ff4 from '../img/1/4.png'
+import ff1 from '../img/1/1.png'
+import ff2 from '../img/1/2.png'
+import ff3 from '../img/1/3.png'
+import ff4 from '../img/1/4.png'
+import ff1 from '../img/1/1.png'
+import ff2 from '../img/1/2.png'
+import ff3 from '../img/1/3.png'
+import ff4 from '../img/1/4.png'
+import ff1 from '../img/1/1.png'
+import ff2 from '../img/1/2.png'
+import ff3 from '../img/1/3.png'
+import ff4 from '../img/1/4.png'
+import ff1 from '../img/1/1.png'
+import ff2 from '../img/1/2.png'
+import ff3 from '../img/1/3.png'
+import ff4 from '../img/1/4.png'
+ */
 const cities = [
     {
         text: 'Museu FF',
@@ -14,220 +103,220 @@ const cities = [
     {
         text: 'Museu do Açucar',
         coordinates: [-16.907017027716385, 32.648311559004235],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [macucar1,macucar2,macucar3,macucar4,macucar5],
         title: 'Museu A Cidade do Açúcar'
         
     },
     {
         text: 'Jardim Botanico',
         coordinates: [ -16.894732944151812, 32.66254156439047],
-        imgURL: ['https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/01/botanical-gardens-madeira-sunset-1.jpg','https://images.turismoenportugal.org/Jardim-Botanico-da-Madeira-Funchal.jpg', 'https://telefericojardimbotanico.com/wp-content/uploads/2016/02/shutterstock_419493172-1024x683.jpg', 'https://ifcn.madeira.gov.pt/images/JB/infoVisit/InfograficoJBM_Peq.jpg', 'https://img.freepik.com/premium-photo/botanical-garden-funchal-madeira-portugal_266870-6399.jpg', 'http://s3-eu-west-1.amazonaws.com/bestguide-images/p/post_images/images/000/028/220/original/J.Botanico9.jpg?1440098412'],
+        imgURL: [jbotan1,jbotan2,jbotan3,jbotan4,jbotan5,jbotan6],
         title: 'Jardim Botânico da Madeira'
     },
     {
         text: 'Blandys Wine Lodge',
         coordinates: [  -16.910624726039863, 32.64787574755793],
-        imgURL: ['https://www.blandyswinelodge.com/wp-content/uploads/2022/09/bwl-Prancheta-1.jpg','https://static.portugalbywine.com/media//MULTIMEDIA/FOTOS/4342/19055625085412p_1920.jpg','https://www.blandyswinelodge.com/wp-content/uploads/2022/09/bwl-Prancheta-5.jpg','https://www.blandyswinelodge.com/wp-content/uploads/2022/09/bwl-Prancheta-2.jpg','https://marshmallowtravels.com/wp-content/uploads/2017/04/IMG_0272-1-1170x550.jpg'],
+        imgURL: [wine1,wine2,wine3,wine4,wine5],
         title: "Blandy's Wine Lodge"
     },
     {
         text: 'Ruinas de Sao Jorge',
         coordinates: [  -16.897686298163855, 32.829943827981865],
-        imgURL: [ 'https://santanamadeirabiosfera.pt/images/Conhecer/Freguesias/Sao_Jorge/Portico_Calhau_de_S_Jorge_-_Duarte_Sousa.jpg','https://i.pinimg.com/originals/63/c6/3d/63c63dd2315e6446e8545cb548d8a43e.jpg'],
+        imgURL: [sjorge1,sjorge2,sjorge3,sjorge4,sjorge5],
         title: 'Ruínas de São Jorge'
     },
     {
         text: 'Parque Tematico',
         coordinates: [  -16.88439591124723,32.8032877283233],
-        imgURL: ['http://s3-eu-west-1.amazonaws.com/bestguide-images/p/post_images/images/000/028/531/original/Parque-Tematico.jpg?1440098833', 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/75/52/a4/casas-tipicas-de-santana.jpg?w=1200&h=-1&s=1','https://images.turismoenportugal.org/Parque-Tematico-da-Madeira.jpg'],
+        imgURL: [ptema1,ptema2,ptema3,ptema4,ptema5,ptema6,ptema7,ptema8],
         title: 'Parque Temático da Madeira'
     },
     {
         text: 'Cristo Rei',
         coordinates: [-16.850579490542646, 32.63854322204222],
-        imgURL: [ 'https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/01/cristo-rei-sunrise-madeira-800x534.jpg','https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/08/christ-statue-madeira-drone.jpg', 'https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/08/cristo-rei-madeira-drone-800x500.jpg', 'https://h8r3x6j3.rocketcdn.me/wp-content/uploads/2021/08/benches-sunset-800x500.jpg', 'https://madeira.best/storage/uploads/img/cristo-rei-viewpoint-miradouro-gajarau-canio-madeira-45bf5783facb41.JPG'],
+        imgURL: [cristo1,cristo2,cristo3,cristo4,cristo5,cristo6,cristo7],
         title: 'Miradouro do Cristo Rei'
     },
     {
         text: 'Colegio dos Jesuitas',
         coordinates: [-16.90923651714468, 32.650222383939635],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [cjesuit1,cjesuit2,cjesuit3],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Cabo Girao',
         coordinates: [  -17.004332248643756, 32.65560776357376],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [cabogi1,cabogi2,cabogi3,cabogi4],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Miradouro Eiro do Serrado',
         coordinates: [ -16.96553329994667, 32.71047170121687],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [eiraserrado1,eiraserrado2,eiraserrado3,eiraserrado4],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Pico do Arieiro',
         coordinates: [-16.928891960936674,32.736242064990336],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [picoar1,picoar2,picoar3 ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Capela do Corpo Santo',
         coordinates: [-16.900322834382155,32.649689400499085],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [corposanto1,corposanto2 ],        
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Casas de Santana',
         coordinates: [-16.882610728836077, 32.80563542576975],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [santanacasa1,santanacasa2,santanacasa3 ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Portas da Cidade',
         coordinates: [-16.906796917144778, 32.64772095671906],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [potascidade1,potascidade2 ],
         title: 'Museu A Cidade do Açúcar'
     },    
     {
         text: 'Palacio de Sao Lourenco',
         coordinates: [-16.91004508645616,32.647380522432314],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [saolourenco1,saolourenco2,saolourenco3,saolourenco4 ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Pilar de Banger',
         coordinates: [-16.908616072964442, 32.64689432191945],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ banger1,banger2],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Estatua Joao Gonçalves Zarco',
         coordinates: [-16.909741272964357,32.648173555667434],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ zarco1,zarco2,zarco3],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Camara Municipal de Santana',
         coordinates: [-16.882250215288973, 32.80558348910334],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [camarasantana1,camarasantana2 ],
         title: 'Museu A Cidade do Açúcar'
     },{
         text: 'Camara Municipal do Funchal',
         coordinates: [-16.90806498465569, 32.650474200977136],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },{
         text: 'Igreja da Sé',
         coordinates: [-16.90832551714482, 32.64822131771056],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },{
         text: 'Levada dos Tornos',
         coordinates: [ -16.8459748846594, 32.67533603415975],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },{
         text: 'Largo da Achada',
         coordinates: [-16.84548341677708, 32.67975709695652],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Quinta do Santo ',
         coordinates: [ -16.817856370211647, 32.72621229336328],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Miradouro do Pico do Facho',
         coordinates: [-16.75869229077785, 32.724084148935745],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Igreja de Nossa Senhora da Conceição',
         coordinates: [-16.766106387053462, 32.7188316749214],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Capela de Nossa Senhora da Piedade',
         coordinates: [-16.713269923481167, 32.74292647036701],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu da Baleia',
         coordinates: [-16.74047684035708, 32.735885669641576],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Furna Cave',
         coordinates: [ -16.69382834769262, 32.75063088651697],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Praia do Faial',
         coordinates: [ -16.85001408002769, 32.792564811578735],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
     {
         text: 'Museu do Açucar',
         coordinates: [ -16.907069746006854, 32.64850512124363],
-        imgURL: [ 'https://cultura.madeira.gov.pt/images/OutrosMuseus/MuseuAcucar2.jpg'],
+        imgURL: [ ],
         title: 'Museu A Cidade do Açúcar'
     },
 
